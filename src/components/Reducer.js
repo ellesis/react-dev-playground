18416e6b3f9f53003e3b1c5445c7cd2c73d862be
Lisa Han <ellesis@gmail.com>
2021-07-03 06:11:22 -0800

    Add action type

--- src/components/Reducer.js
+++ src/components/Reducer.js
@@ -3,8 +3,20 @@ import React, {useState, useReducer} from 'react'
 // Dispatch - State Update
 // Action - Request State Update
 // Reducer - Request info
+const ACTION_TYPE = {
+  deposit: 'deposit',
+  withdraw: 'withdraw'
+}
 const reducer = (state, action) => {
   console.log('reducer working', state, action)
+  switch (action.type) {
+    case ACTION_TYPE.deposit:
+      return state + action.payload
+    case ACTION_TYPE.withdraw:
+      return state - action.payload
+    default:
+      return state
+  }
 }
 
 export default function Reducer() {
@@ -14,16 +26,22 @@ export default function Reducer() {
   return (
     <div>
       <h2>useReducer Bank</h2>
-      <p>total: {money}</p>
+      <p>total: $ {money}</p>
       <input type="number" value={number} onChange={(e) => setNumber(parseInt(e.target.value))} step="1000" />
       <button
         onClick={() => {
-          dispatch({type: 'deposit', payload: number})
+          dispatch({type: ACTION_TYPE.deposit, payload: number})
         }}
       >
         input
       </button>
-      <button>output</button>
+      <button
+        onClick={() => {
+          dispatch({type: ACTION_TYPE.withdraw, payload: number})
+        }}
+      >
+        output
+      </button>
     </div>
   )
 }
